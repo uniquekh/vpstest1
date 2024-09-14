740da1ba6c6b152bd91301fdfa4dfec3ae309b8e
Unique <mujeem000@gmail.com>
2024-09-14 18:06:18 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.9.2-slim-buster
+RUN mkdir /app && chmod 777 /app
+WORKDIR /app
+ENV DEBIAN_FRONTEND=noninteractive
+RUN apt -qq update && apt -qq install -y git python3 python3-pip ffmpeg
+COPY . .
+RUN pip3 install --no-cache-dir -r requirements.txt
+CMD gunicorn app:app & python3 -m main
+
---
+++ main/__init__.py
@@ -0,0 +1,32 @@
+#Github.com/Vasusen-code
+
+from pyrogram import Client
+
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
+from decouple import config
+import logging, time, sys
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+# variables
+API_ID = config("API_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+
+bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+
+Bot = Client(
+    "SaveRestricted",
+    bot_token=BOT_TOKEN,
+    api_id=int(API_ID),
+    api_hash=API_HASH
+)    
+
+try:
+    Bot.start()
+except Exception as e:
+    print(e)
+    sys.exit(1)
